62c90c4be25917a580b53aed90fcb8034cebfac2
Jeremy Paul <jeremyjcpaul@gmail.com>
2023-02-07 15:21:05 +0000

    Initial reorganising of project
     - Removes the Java and Python folders, make the JS folder the root
     - Adds the assessment spec and started a README
     - .gitignore
     - Adds a dummy Mocha test to check node Github workflow is working

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cinema Tickets - JavaScript Assessment
+
+To run this repo:
+
+- run `npm install` to install
+- run `npm test` to run the tests
+
+---
---
+++ test/dummy.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Dummy test', function () {
+	it('should just pass the test', function () {
+		assert.equal(1, 1)
+	});
+});
